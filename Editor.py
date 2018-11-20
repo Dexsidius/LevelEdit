d2ab6a241d54bd021b0cb2f40e6165ca1d6d8933
isaboll1 <isaboll12@gmail.com>
2018-11-20 10:18:56 -0500

    Initial Commit that includes all of the things needed for the editor.

---
+++ Editor.py
@@ -0,0 +1,11 @@
+import os
+os.environ["PYSDL2_DLL_PATH"] = os.path.dirname(os.path.abspath(__file__))
+from sdl2 import *
+from sdl2.sdlttf import *
+import ctypes
+#Editor by Sardonicals
+
+def main():
+    pass
+    
+main()
